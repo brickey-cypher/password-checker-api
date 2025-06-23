ae4d96ee478e0d207c94fac4a031e06eabd545f1
brickey-cypher <brimarie7797@gmail.com>
2025-06-22 22:52:34 -0500

    fix build error

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ curl -L https://github.com/nlohmann/json/releases/download/v3.11.3/json.hpp -o i
 # Compile the checker with JSON support
 g++ -std=c++17 checker.cpp -o checker -Iinclude
 
+chmod +x checker
+
